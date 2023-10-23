ec66a7d9313221796527d616db81d0b08a74cd62
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-23 15:08:16 +0000

    actualizacion

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,38 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name === null) {
+    variables.name = "Nyarlathotep";
+  }
+  if (variables.lastName === null) {
+    variables.lastName = "";
+  }
+  if (variables.role === null) {
+    variables.role = "El Caos Reptante";
+  }
+  if (variables.country === null) {
+    variables.country = "Desconocido";
+  }
+  if (variables.city === null) {
+    variables.city = "El caos final más allá de la existencia ordinaria";
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter ||
+              ""} "><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              ""}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin ||
+              ""}/"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              ""}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -52,11 +71,14 @@ function render(variables = {}) {
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
+    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://socialecologies.files.wordpress.com/2022/11/nyarlathotep_2.png?w=458&h=266",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://socialecologies.files.wordpress.com/2022/11/nyarlohotep_6a.png?w=2000&h=&crop=1",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
